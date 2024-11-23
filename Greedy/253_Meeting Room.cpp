7cc948696d27ddede729e334248ecc27ff5a8f25
sehyunkim <sehyun.seankim@gmail.com>
2024-11-23 15:11:26 -0500

    253 - simplified (still wrong)

--- Greedy/253_Meeting Room.cpp
+++ Greedy/253_Meeting Room.cpp
@@ -10,33 +10,20 @@ public:
         // Sort by the first element
         sort(intervals.begin(), intervals.end());
         int N = intervals.size();
+
         int cnt = 1;
-        pair<int, int> range = {intervals[0][0], intervals[0][1]};
-        int earliest_end = range.second;
-        int max_cnt = cnt;
-        for(int i=1; i < N; i++) {
-            int int_start = intervals[i][0];
-            int int_end = intervals[i][1];
-            if (int_start < range.second) {
-                if (int_start < earliest_end) {
-                    cnt++;
-                }
-                else {
-                    range.first = int_start;
-                }
-                earliest_end = min(range.second, int_end);
-                range.second = max(range.second, int_end);
+        int earliest_end = intervals[0][1];
+
+        for (int i=1; i < N; i++) {
+            if (intervals[i][0] < earliest_end) {
+                cnt++;
+                earliest_end = min(earliest_end, intervals[i][1]);
             }
             else {
-                range.first = int_start;
-                range.second = int_end;
-                earliest_end = range.second;
-                cnt = 1;
+                earliest_end = intervals[i][1];
             }
-            
-            max_cnt = max(max_cnt, cnt);
         }
-        return max_cnt;
+        return cnt;
     }
 };
 
@@ -46,6 +33,7 @@ int main()
     vector<vector<int>> intervals = {{0,30}, {5,10}, {15,20} };
     vector<vector<int>> intervals2 = {{7, 10}, {2, 4} };
     vector<vector<int>> intervals3 = {{1,8},{6,20},{9,16},{13,17}};
+    vector<vector<int>> intervals4 = {{2,15},{36,45},{9,29},{16,23},{4,9}};
     cout << sol.minMeetingRooms(intervals) << endl;
     cout << sol.minMeetingRooms(intervals2) << endl;
     cout << sol.minMeetingRooms(intervals3) << endl;
