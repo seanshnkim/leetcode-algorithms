5417dc608b960742084788727380706cea2db125
sehyunkim <sehyun.seankim@gmail.com>
2024-12-01 10:02:37 -0500

    376 better solution

--- Greedy/376_Wiggle Subsequence.cpp
+++ Greedy/376_Wiggle Subsequence.cpp
@@ -10,20 +10,24 @@ public:
         if (nums.size() < 2)
             return nums.size();
         
-        vector<int> up(nums.size(), 0);
-        vector<int> down(nums.size(), 0);
+        vector<int> up(nums.size());
+        vector<int> down(nums.size());
+        up[0] = down[0] = 1;
         
         for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    up[i] = max(up[i], down[j] + 1);
-                } else if (nums[i] < nums[j]) {
-                    down[i] = max(down[i], up[j] + 1);
-                }
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                down[i] = down[i - 1];
+                up[i] = up[i - 1];
             }
         }
         
-        return 1 + max(down[nums.size() - 1], up[nums.size() - 1]);
+        return max(down[nums.size() - 1], up[nums.size() - 1]);
     }
 };
 
@@ -37,6 +41,6 @@ int main()
     cout << sol.wiggleMaxLength(nums1) << endl;
     cout << sol.wiggleMaxLength(nums2) << endl;
     cout << sol.wiggleMaxLength(nums3) << endl;
-    
+
     return 0;
 }
