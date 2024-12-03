820b8204e14f63ae32bd1356c9733c2f8fb7ff9c
Sean Kim <sehyun.seankim@gmail.com>
2024-12-03 11:05:01 -0500

    152 DP

--- Dynamic-Programming/152_Maximum Product Subarray.cpp
+++ Dynamic-Programming/152_Maximum Product Subarray.cpp
@@ -8,12 +8,64 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int N = nums.size();
-        vector<int> dp(N, 0);
-        dp[0] = nums[0];
-        for (int i=1; i < N; i++){
-            dp[i] = max(nums[i]*dp[i-1], nums[i]);
+        // first = positive, second = negative
+        vector<pair<int, int>> dp(N);
+        if (nums[0] > 0) {
+            dp[0].first = nums[0];
+            dp[0].second = 0;
         }
-        return *max_element(dp.begin(), dp.end());
+        else if (nums[0] < 0) {
+            dp[0].first = 0;
+            dp[0].second = nums[0];
+        }
+        else {
+            dp[0].first = 0;
+            dp[0].second = 0;
+        }
+        dp[0] = {nums[0], 0};
+        for (int i=1; i < N; i++){
+            if (nums[i] < 0) {
+                if (dp[i-1].first == 0 && dp[i-1].second < 0) {
+                    dp[i].first  = dp[i-1].second * nums[i];
+                    dp[i].second = nums[i];
+                }
+                else if (dp[i-1].first > 0 && dp[i-1].second == 0) {
+                    dp[i].first  = 0;
+                    dp[i].second = dp[i-1].first * nums[i];
+                }
+                else if (dp[i-1].first > 0 && dp[i-1].second < 0) {
+                    dp[i].first  = dp[i-1].second * nums[i];
+                    dp[i].second = dp[i-1].first * nums[i];
+                }
+                else {
+                    dp[i].first = 0;
+                    dp[i].second = nums[i];
+                }
+            }
+            else if (nums[i] > 0) {
+                if (dp[i-1].first == 0 && dp[i-1].second < 0) {
+                    dp[i].first  = nums[i];
+                    dp[i].second = dp[i-1].second * nums[i];
+                }
+                else if (dp[i-1].first > 0 && dp[i-1].second == 0) {
+                    dp[i].first  = dp[i-1].first * nums[i];
+                    dp[i].second = 0;
+                }
+                else if (dp[i-1].first > 0 && dp[i-1].second < 0) {
+                    dp[i].first  = dp[i-1].first * nums[i];
+                    dp[i].second = dp[i-1].second * nums[i];
+                }
+                else {
+                    dp[i].first = nums[i];
+                    dp[i].second = 0;
+                }
+            }
+        }
+        int max_product = nums[0];
+        for (int i=1; i < N; i++) {
+            max_product = max(dp[i].first, max_product);
+        }
+        return max_product;
     }
 };
 
