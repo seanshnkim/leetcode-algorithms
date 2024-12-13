a85e8f3e8332bb4b59db567454d8795a8b26a583
sehyunkim <sehyun.seankim@gmail.com>
2024-12-13 09:29:19 -0500

    110 - Balanced Binary Tree (1st)

---
+++ Graph/110_Balanced Binary Tree.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        if (!isBalanced(root->left) || !isBalanced(root->right)) {
+            return false;
+        }
+        int diff = dfs(root->left, 1) - dfs(root->right, 1);
+        return (-1 <= diff && diff <= 1);
+    }
+
+    int dfs(TreeNode* root, int depth) {
+        if (!root) {
+            return depth-1;
+        }
+        return max(dfs(root->left, depth+1), dfs(root->right, depth+1) );
+    }
+};
+
+int main()
+{
+    Solution sol;
+    TreeNode root;
+    cout << sol.isBalanced(&root) << endl;
+    return 0;
+}
