2832f321e977d460cb43f713189754ad79911db3
sehyunkim <sehyun.seankim@gmail.com>
2024-12-29 12:19:00 -0500

    116 BFS

--- Graph/116_Populating Next Right Pointers in Each Node.cpp
+++ Graph/116_Populating Next Right Pointers in Each Node.cpp
@@ -9,11 +9,41 @@ struct Node {
   Node *left;
   Node *right;
   Node *next;
+  Node(int x) : val(x), left(nullptr), right(nullptr), next(nullptr) {}
 };
+
+Node* createPerfectBinaryTree(const vector<int>& arr) {
+    if (arr.empty()) {
+        return nullptr;
+    }
+
+    Node* root = new Node(arr[0]);
+    queue<Node*> q;
+    q.push(root);
+
+    int i = 1;
+    while (i < arr.size()) {
+        Node* current = q.front();
+        q.pop();
+
+        if (i < arr.size()) {
+            current->left = new Node(arr[i++]);
+            q.push(current->left);
+        }
+
+        if (i < arr.size()) {
+            current->right = new Node(arr[i++]);
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
 
 class Solution {
 public:
     Node* connect(Node* root) {
+        if(!root) {return NULL;}
         queue<Node*> q;
         q.push(root);
 
@@ -23,6 +53,10 @@ public:
             Node* prevNode = q.front();
             q.pop();
             qSize--;
+            if (prevNode->left && prevNode->right) {
+                q.push(prevNode->left);
+                q.push(prevNode->right);
+            }
             while (qSize--) {
                 Node* curNode = q.front();
                 q.pop();
@@ -46,6 +80,8 @@ public:
 int main()
 {
     Solution sol;
-    // cout << sol.connect() << endl;
+    vector <int> root_vector = {1,2,3,4,5,6,7};
+    Node* root = createPerfectBinaryTree(root_vector);
+    Node* root_result = sol.connect(root);
     return 0;
 }
